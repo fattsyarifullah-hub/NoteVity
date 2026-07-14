c3706fcd153fd52c5e17486811ba0a0938fe6de6
Fathan Syarifullah <syarifullahfathan@gmail.com>
2026-07-14 14:39:47 +0700

    feat(screen home): tambahkan fitur completed dan on progress di daily task

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
+import '../widgets/daily_task.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(child: Text("Ini container 1"),),
-        Container(child: Text("Ini container 2"),),
-        Container(child: Text("Ini container 3"),)
-      ],
-    );
+    return DailyTask();
   }
 }
--- lib/widgets/popup_calendar.dart
+++ lib/widgets/popup_calendar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../providers/task_providers.dart';
 import 'package:provider/provider.dart';
 import '../models/task_model.dart';
+import 'dart:async';
 
 class PopupCalendar extends StatefulWidget {
   const PopupCalendar({super.key});
@@ -18,9 +19,12 @@ class _PopupCalendarState extends State<PopupCalendar> {
 
   TimeOfDay _selectedEndTime = TimeOfDay.now();
 
+  Timer? _debounce;
+
   @override
   void dispose() {
     _titleController.dispose();
+    _debounce?.cancel();
     super.dispose();
   }
 
@@ -28,8 +32,7 @@ class _PopupCalendarState extends State<PopupCalendar> {
     final TimeOfDay? picked = await showTimePicker(
       context: context,
       initialTime: _selectedEndTime,
-      initialEntryMode: TimePickerEntryMode.input
-      
+      initialEntryMode: TimePickerEntryMode.input,
     );
     if (picked != null) {
       setState(() {
@@ -61,6 +64,13 @@ class _PopupCalendarState extends State<PopupCalendar> {
     Navigator.of(context).pop();
   }
 
+  void lazyInput(String input) {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(Duration(seconds: 3), () {
+      print(input);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -69,6 +79,7 @@ class _PopupCalendarState extends State<PopupCalendar> {
         children: [
           TextField(
             controller: _titleController,
+            onChanged: lazyInput,
             decoration: InputDecoration(labelText: "Masukin aktivitas LOH"),
           ),
           SizedBox(height: 10),
@@ -87,13 +98,12 @@ class _PopupCalendarState extends State<PopupCalendar> {
           DropdownButton<TaskPriority>(
             value: _selectedPriority,
             isExpanded: true,
-            items: TaskPriority.values
-              .map((priority) {
-                return DropdownMenuItem(
-                  value: priority,
-                  child: Text(priority.name),
-                );
-              }).toList(),
+            items: TaskPriority.values.map((priority) {
+              return DropdownMenuItem(
+                value: priority,
+                child: Text(priority.name),
+              );
+            }).toList(),
             onChanged: (val) {
               if (val != null)
                 setState(() {
@@ -105,13 +115,10 @@ class _PopupCalendarState extends State<PopupCalendar> {
       ),
       actions: [
         TextButton(
-          onPressed: () => Navigator.pop(context), 
-          child: Text("Batal")
+          onPressed: () => Navigator.pop(context),
+          child: Text("Batal"),
         ),
-        ElevatedButton(
-          onPressed: () => submitData(), 
-          child: Text("Simpan")
-        )
+        ElevatedButton(onPressed: () => submitData(), child: Text("Simpan")),
       ],
     );
   }
